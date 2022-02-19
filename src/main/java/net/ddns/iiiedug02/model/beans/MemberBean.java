a044a455fed73442c8f026ca2d9541c2d6affb38
1+ <nilm987521@gmail.com>
2022-02-19 16:59:10 +0800

    配合資料庫修正

--- src/main/java/net/ddns/iiiedug02/model/beans/MemberBean.java
+++ src/main/java/net/ddns/iiiedug02/model/beans/MemberBean.java
@@ -1,81 +1,108 @@
 package net.ddns.iiiedug02.model.beans;
 
 import java.io.Serializable;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "Members")
+@Table(name = "members")
 public class MemberBean implements Serializable {
 
   private static final long serialVersionUID = 1L;
 
   @Id
+  @Column(name = "uid")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int uid;
+
+
   @Column(name = "username")
   private String username;
   @Column(name = "password")
   private String password;
   @Column(name = "auth")
-  private String auth = "nuser";
-  @Column(name = "activated")
+  private String auth;
+  @Column(name = "activate")
   private short activated = 1; // 0:true, 1:false
 
 
-  // @OneToOne(fetch = FetchType.LAZY, mappedBy = "member", cascade = CascadeType.ALL)
-  // private MemberDetailsBean memberDetail;
+  @OneToOne(fetch = FetchType.LAZY, mappedBy = "member", cascade = CascadeType.ALL)
+  private MemberDetailsBean memberDetail;
+
+
+  public int getUid() {
+    return uid;
+  }
+
+
+  public void setUid(int uid) {
+    this.uid = uid;
+  }
 
 
   public String getUsername() {
     return username;
   }
 
-  public MemberBean setUsername(String username) {
+
+  public void setUsername(String username) {
     this.username = username;
-    return this;
   }
+
 
   public String getPassword() {
     return password;
   }
 
-  public MemberBean setPassword(String password) {
+
+  public void setPassword(String password) {
     this.password = password;
-    return this;
   }
+
 
   public String getAuth() {
     return auth;
   }
 
-  public MemberBean setAuth(String auth) {
+
+  public void setAuth(String auth) {
     this.auth = auth;
-    return this;
   }
+
 
   public short getActivated() {
     return activated;
   }
 
-  public MemberBean setActivated(short activated) {
+
+  public void setActivated(short activated) {
     this.activated = activated;
-    return this;
   }
 
-  // public MemberDetailsBean getMemberDetail() {
-  // return memberDetail;
-  // }
-  //
-  // public MemberBean setMemberDetail(MemberDetailsBean memberDetail) {
-  // this.memberDetail = memberDetail;
-  // return this;
-  // }
+
+  public MemberDetailsBean getMemberDetail() {
+    return memberDetail;
+  }
+
+
+  public void setMemberDetail(MemberDetailsBean memberDetail) {
+    this.memberDetail = memberDetail;
+  }
+
 
   @Override
   public String toString() {
-    return "MemberBean [username=" + username + ", password=" + password + ", auth=" + auth
-        + ", activated=" + activated + "]";
+    return "MemberBean [uid=" + uid + ", username=" + username + ", password=" + password
+        + ", auth=" + auth + ", activated=" + activated + "]";
   }
+
+
 
 }
--- src/main/java/net/ddns/iiiedug02/model/beans/MemberDetailsBean.java
+++ src/main/java/net/ddns/iiiedug02/model/beans/MemberDetailsBean.java
@@ -4,24 +4,27 @@ import java.io.Serializable;
 import java.sql.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
 
 @Entity
-@Table(name = "MemberDetails")
+@Table(name = "memberDetails")
 public class MemberDetailsBean implements Serializable {
 
   private static final long serialVersionUID = 2L;
 
   @GenericGenerator(name = "generator", strategy = "foreign",
-      parameters = @Parameter(name = "property", value = "members"))
+      parameters = @Parameter(name = "property", value = "member"))
   @Id
-  @Column(name = "username")
-  @GeneratedValue(generator = "SharedPrimaryKeyGenerator")
-  private String username;
+  @Column(name = "uid")
+  @GeneratedValue(generator = "generator")
+  private int uid;
 
 
   @Column(name = "address")
@@ -38,80 +41,98 @@ public class MemberDetailsBean implements Serializable {
   private String job;
 
 
-  // @OneToOne(fetch = FetchType.LAZY)
-  // @PrimaryKeyJoinColumn(name = "username", referencedColumnName = "username")
-  // private MemberBean member;
+  @OneToOne(fetch = FetchType.LAZY)
+  @PrimaryKeyJoinColumn
+  private MemberBean member;
 
-  public String getUsername() {
-    return username;
+
+  public int getUid() {
+    return uid;
   }
 
-  public void setUsername(String username) {
-    this.username = username;
+
+  public void setUid(int uid) {
+    this.uid = uid;
   }
+
 
   public String getAddress() {
     return address;
   }
 
+
   public void setAddress(String address) {
     this.address = address;
   }
+
 
   public String getPhone() {
     return phone;
   }
 
+
   public void setPhone(String phone) {
     this.phone = phone;
   }
 
-  // public MemberBean getMember() {
-  // return member;
-  // }
-  //
-  // public void setMember(MemberBean member) {
-  // this.username = member.getUsername();
-  // this.member = member;
-  // }
 
   public String getFullname() {
     return fullname;
   }
 
+
   public void setFullname(String fullname) {
     this.fullname = fullname;
   }
+
 
   public String getEmail() {
     return email;
   }
 
+
   public void setEmail(String email) {
     this.email = email;
   }
+
 
   public Date getBirthday() {
     return birthday;
   }
 
+
   public void setBirthday(Date birthday) {
     this.birthday = birthday;
   }
+
 
   public String getJob() {
     return job;
   }
 
+
   public void setJob(String job) {
     this.job = job;
   }
 
+
+  public MemberBean getMember() {
+    return member;
+  }
+
+
+  public void setMember(MemberBean member) {
+    this.member = member;
+  }
+
+
   @Override
   public String toString() {
-    return "MemberDetailsBean [username=" + username + ", address=" + address + ", phone=" + phone
+    return "MemberDetailsBean [uid=" + uid + ", address=" + address + ", phone=" + phone
         + ", fullname=" + fullname + ", email=" + email + ", birthday=" + birthday + ", job=" + job
         + "]";
   }
+
+
 
 }
