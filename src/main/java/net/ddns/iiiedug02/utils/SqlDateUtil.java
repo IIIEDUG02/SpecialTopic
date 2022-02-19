89c46289c9caf5c57c688d6a8e5061c4e161ba2f
1+ <nilm987521@gmail.com>
2022-02-19 16:53:35 +0800

    新增 SQL Date型態轉換工具

---
+++ src/main/java/net/ddns/iiiedug02/utils/SqlDateUtil.java
@@ -0,0 +1,18 @@
+package net.ddns.iiiedug02.utils;
+
+import java.text.SimpleDateFormat;
+
+public class SqlDateUtil {
+  public static java.sql.Date strToDate(String strDate) {
+    String str = strDate;
+    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+    java.util.Date d = null;
+    try {
+      d = format.parse(str);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    java.sql.Date date = new java.sql.Date(d.getTime());
+    return date;
+  }
+}
