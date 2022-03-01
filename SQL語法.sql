d4dd201897b291cfe0e70bc246827d67168434ed
RT <ab101201977@gmail.com>
2022-03-01 22:32:53 +0800

    SQL語法修正終極2.0版

--- SQL語法.sql
+++ SQL語法.sql
@@ -2,8 +2,8 @@ create table Members(
 id integer primary key not null identity(1,1),
 [username] [nvarchar](50) NOT NULL UNIQUE,
 [password] [nvarchar](50) NOT NULL,
-[auth] [nvarchar](10) NULL,
-[activated] [char](10) NULL
+[roles] [nvarchar](10) NULL,
+[activated] [bit] NULL
 );
 
 create table MemberDetails(
@@ -15,7 +15,7 @@ phone varchar(10)not null,
 birthday date not null,
 job nvarchar(20) not null,
 paypalacc int NULL,
-bankcord int NULL,
+bankcode int NULL,
 bankacc int NULL,
 FOREIGN KEY (id) REFERENCES Members(id)
 );
