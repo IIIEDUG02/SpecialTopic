8e5b1d1f604f784cb167742aed27d548974006d8
1+ <nilm987521@gmail.com>
2022-03-04 21:06:58 +0800

    新增有關Login的Controller

---
+++ src/main/java/net/ddns/iiiedug02/controller/LoginController.java
@@ -0,0 +1,13 @@
+package net.ddns.iiiedug02.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+  @GetMapping("login")
+  public String login() {
+    return "loginForm";
+  }
+}
