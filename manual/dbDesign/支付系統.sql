b47e18cef4ae49bbc9233014c031e5a1f67c52fb
1+ <nilm987521@gmail.com>
2022-03-04 09:51:36 +0800

    SQL更新（細節如檔內說明

--- manual/dbDesign/支付系統.sql
+++ manual/dbDesign/支付系統.sql
@@ -1,15 +1,21 @@
 -- 學生購課紀錄
+-- 2022/3/3更新說明：因為之前是用複合主鍵，在設定Hibernate @Annotation極為複雜，故僅設tid為pk，把uid+cid做複合唯一
+--                 Hibernate在執行CRUD時，會預設使用 order_date作為欄位名，導致欄位找不到
+use db;
 CREATE TABLE c2b (
-	tid int IDENTITY(1,1) not null,
-    sid int not null REFERENCES members(uid) ,
+	tid int unique IDENTITY(1,1) not null,
+    uid int not null REFERENCES members(uid) ,
     cid int not null REFERENCES class(cid) ,
-    completed bit not null DEFAULT 0
-    primary key(tid,sid,cid)
+    order_date datetime not null,
+    completed bit not null DEFAULT 0,
+    primary key(tid),
+    UNIQUE (uid,cid)
 );
 
 -- 公司支付老師薪資紀錄
 CREATE TABLE b2c (
 	tid int not null REFERENCES c2b(tid),
-	tid int not null REFERENCES members(uid) ,
+	order_date datetime not null,
 	completed bit not null DEFAULT 0
+	primary key(tid)
 );
