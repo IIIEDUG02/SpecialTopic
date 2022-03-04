76a721d0e7ec46c63a53978cd21e62f1a0041758
1+ <nilm987521@gmail.com>
2022-03-04 21:11:59 +0800

    檔案搬移

--- src/main/java/net/ddns/iiiedug02/controller/demo/MemberController.java
+++ src/main/java/net/ddns/iiiedug02/controller/demo/MemberController.java
@@ -1,39 +1,39 @@
-package net.ddns.iiiedug02.controller.demo;
-
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
-import net.ddns.iiiedug02.model.bean.demo.MemberBean;
-import net.ddns.iiiedug02.model.service.demo.MemberService;
-
-
-@Controller
-public class MemberController {
-
-  @Autowired
-  private MemberService ms;
-
-  // @Autowired
-  // private SqlDateUtil sqlDateUtil;
-
-  @ResponseBody
-  @GetMapping("memberFindById")
-  public MemberBean memberFindById() {
-    return ms.findById(1);
-  }
-
-  @ResponseBody
-  @GetMapping("memberFindAll")
-  public List<MemberBean> memberFindAll() {
-    return ms.findAll();
-  }
-
-  @ResponseBody
-  @GetMapping("memberFindByUsername/{username}")
-  public String memberFindByUsername(@PathVariable("username") String username) {
-    return ms.findByUsername(username).toString();
-  }
-}
+// package net.ddns.iiiedug02.controller.demo;
+//
+// import java.util.List;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.ResponseBody;
+// import net.ddns.iiiedug02.model.bean.MemberBean;
+// import net.ddns.iiiedug02.model.service.MemberService;
+//
+//
+// @Controller
+// public class MemberController {
+//
+// @Autowired
+// private MemberService ms;
+//
+// // @Autowired
+// // private SqlDateUtil sqlDateUtil;
+//
+// @ResponseBody
+// @GetMapping("memberFindById")
+// public MemberBean memberFindById() {
+// return ms.findById(1);
+// }
+//
+// @ResponseBody
+// @GetMapping("memberFindAll")
+// public List<MemberBean> memberFindAll() {
+// return ms.findAll();
+// }
+//
+// @ResponseBody
+// @GetMapping("memberFindByUsername/{username}")
+// public String memberFindByUsername(@PathVariable("username") String username) {
+// return ms.findByUsername(username).toString();
+// }
+// }
--- src/main/java/net/ddns/iiiedug02/model/interfaces/demo/MemberRepository.java
+++ src/main/java/net/ddns/iiiedug02/model/interfaces/demo/MemberRepository.java
@@ -3,7 +3,7 @@ package net.ddns.iiiedug02.model.interfaces.demo;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import net.ddns.iiiedug02.model.bean.demo.MemberBean;
+import net.ddns.iiiedug02.model.bean.MemberBean;
 
 public interface MemberRepository extends JpaRepository<MemberBean, Integer> {
   @Query(value = "from MemberBean where username = ?1")
