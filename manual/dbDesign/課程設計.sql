33613f1e459c277447b2c90eda8bb9a791e41764
gt6678 <gt6678@gmail.com>
2022-03-14 01:52:11 +0800

    資料表更動0314

--- manual/dbDesign/課程設計.sql
+++ manual/dbDesign/課程設計.sql
@@ -1,26 +1,31 @@
+
+
+
 create table class(
 	cid int IDENTITY(1,1)PRIMARY KEY not null,
 	title nvarchar(50) not null,
-	type nvarchar(50) not null,
+	classType nvarchar(50) not null,
 	price int not null,
-	photo varbinary,
-	uid int not null REFERENCES members(uid)
+	photo varbinary(max),
+	id int not null REFERENCES members(id)
 );
 
-create table orders(
-	oid int IDENTITY(1,1)PRIMARY KEY not null,
-	orderDate datetime not null,
-	cid int not null REFERENCES class(cid),
-	uid int not null REFERENCES members(uid)
+create table curriculum(
+	cuid int IDENTITY(1,1)PRIMARY KEY not null,
+	chapter nvarchar(50) not null,
+	videoPath varchar(200),
+	cuTitle bit default 1 not null,
+	cid int not null REFERENCES class(cid)
+
 );
-
-
 create table classDetails(
 	cid int not null PRIMARY KEY REFERENCES class(cid) ,
 	descript nvarchar(500) not null,
 	needed_tool nvarchar(100) not null,
-	stu_reqired nvarchar(100) not null,
+	stu_required nvarchar(100) not null,
 	length_min int not null,
 	goal nvarchar(200) not null,
-	video varchar(100) not null,
+	video varchar(200) ,
 );
+
+
