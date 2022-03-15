1737b53ec9636f37b15bcc843d2eafffe58cdc31
RT <ab101201977@gmail.com>
2022-03-15 12:03:00 +0800

    1311

--- manual/dbDesign/支付系統.sql
+++ manual/dbDesign/支付系統.sql
@@ -1,21 +1,15 @@
 -- 學生購課紀錄
--- 2022/3/3更新說明：因為之前是用複合主鍵，在設定Hibernate @Annotation極為複雜，故僅設tid為pk，把uid+cid做複合唯一
---                 Hibernate在執行CRUD時，會預設使用 order_date作為欄位名，導致欄位找不到
-use db;
 CREATE TABLE c2b (
-	tid int unique IDENTITY(1,1) not null,
-    uid int not null REFERENCES members(uid) ,
+	tid int IDENTITY(1,1) not null,
+    sid int not null REFERENCES members(uid) ,
     cid int not null REFERENCES class(cid) ,
-    order_date datetime not null,
-    completed bit not null DEFAULT 0,
-    primary key(tid),
-    UNIQUE (uid,cid)
+    completed bit not null DEFAULT 0
+    primary key(tid,sid,cid)
 );
 
 -- 公司支付老師薪資紀錄
 CREATE TABLE b2c (
 	tid int not null REFERENCES c2b(tid),
-	order_date datetime not null,
+	tid int not null REFERENCES members(uid) ,
 	completed bit not null DEFAULT 0
-	primary key(tid)
 );
---
+++ src/main/java/net/ddns/iiiedug02/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+package net.ddns.iiiedug02;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import net.ddns.iiiedug02.model.service.MemberService;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  private MemberService authmemberService;
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(authmemberService).passwordEncoder(new BCryptPasswordEncoder());
+  }
+
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    super.configure(web);
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+        .antMatchers(HttpMethod.GET, "/login_page", "/logout_page", "/welcome", "/registerPage1",
+            "/registerPage2", "/getUsername", "/", "/js/**", "/css/**")
+        .permitAll().antMatchers(HttpMethod.POST, "/registerAction1", "/registerAction2")
+        .permitAll().antMatchers("/cashflow/**").hasRole("admin");
+
+    http.authorizeRequests().anyRequest().authenticated();
+
+    http.formLogin().loginPage("/login_page").usernameParameter("username")
+        .passwordParameter("password").defaultSuccessUrl("/").permitAll();
+    http.rememberMe().tokenValiditySeconds(86400).key("rememberMe-key");
+    http.logout().deleteCookies("JESSIONID", "rememberMe-key").logoutUrl("/logout_page");
+
+    http.cors().and().csrf().disable();
+  }
+
+}
