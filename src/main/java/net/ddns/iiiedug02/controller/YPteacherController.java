71f9462bad41b808f9e697401f4d0772184fa52e
LAPTOP-TH8REEM4\TUF GAMING <qwqw512007@gmail.com>
2022-03-15 23:19:09 +0800

    修改YPteachercontroller

--- src/main/java/net/ddns/iiiedug02/controller/YPteacherController.java
+++ src/main/java/net/ddns/iiiedug02/controller/YPteacherController.java
@@ -1,5 +1,6 @@
 package net.ddns.iiiedug02.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -8,8 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import net.ddns.iiiedug02.model.bean.ClassBean;
 import net.ddns.iiiedug02.model.bean.YPteacher;
 import net.ddns.iiiedug02.model.service.CashService;
+import net.ddns.iiiedug02.model.service.ClassBeanService;
 import net.ddns.iiiedug02.model.service.YPteacherService;
 
 
@@ -21,9 +24,10 @@ public class YPteacherController {
     private YPteacherService bService;
 	
 	@Autowired
-    private CashService cService;
+    private CashService cashService;
 	
-	
+	@Autowired
+    private ClassBeanService classService;
 	
 
 	
@@ -34,8 +38,17 @@ public class YPteacherController {
 	
 	@GetMapping("/ypteacherfindAll.controller")
 	public List<YPteacher> processfindAll() {
-		List<Map<String, Integer>> c = cService.getYearTop5Class(2022);
+		List<Map<String, Integer>> cList = cashService.getYearTop5Class(2022);
+		List<ClassBean> cbList = new ArrayList<ClassBean>();
+		for(Map<String, Integer> c : cList) {
+			ClassBean cbBean = classService.findById(c.get("cid"));
+			cbList.add(cbBean);
+		}
+		
+		
 		return bService.findAll();
 	}
+	
+	
 	
 }
