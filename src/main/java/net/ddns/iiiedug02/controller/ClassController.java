2b17a4fe99f25cc6f8772fc7681666c9856307e8
1+ <nilm987521@gmail.com>
2022-03-15 21:10:48 +0800

    m.addAllAttrubutes(XXXX)->m.addAttrubute("YYY",XXX)

--- src/main/java/net/ddns/iiiedug02/controller/ClassController.java
+++ src/main/java/net/ddns/iiiedug02/controller/ClassController.java
@@ -2,17 +2,11 @@ package net.ddns.iiiedug02.controller;
 
 import java.security.Principal;
 import java.util.List;
-
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-
 import net.ddns.iiiedug02.model.bean.ClassBean;
 import net.ddns.iiiedug02.model.bean.Member;
 import net.ddns.iiiedug02.model.service.ClassBeanService;
@@ -20,29 +14,29 @@ import net.ddns.iiiedug02.model.service.MemberService;
 
 @Controller
 public class ClassController {
-	
-	@Autowired
-	private ClassBeanService cbs;
-	
-	@Autowired
-	private MemberService ms;
-	
-	//show課程
-	@GetMapping(value = "/seeteacherclass")
-	public String processShowClass(Principal p,Model m) {
-		Member teacher =ms.findByUsername(p.getName());
-		List<ClassBean> teacherclass = cbs.findAllByCid(teacher.getUid());
-		m.addAllAttributes(teacherclass);
-		
-		return "teacherpage";
-		
-	}
-	
-	//刪除課程
-	@GetMapping(value = "/deleteclass/{cid}")
-	public void deleteClass(@PathVariable int cid) {
-		cbs.deleteById(cid);	
-	}
-	
+
+  @Autowired
+  private ClassBeanService cbs;
+
+  @Autowired
+  private MemberService ms;
+
+  // show課程
+  @GetMapping(value = "/seeteacherclass")
+  public String processShowClass(Principal p, Model m) {
+    Member teacher = ms.findByUsername(p.getName());
+    List<ClassBean> teacherclass = cbs.findAllByCid(teacher.getUid());
+    m.addAttribute("teacherclass", teacherclass);
+
+    return "teacherpage";
+
+  }
+
+  // 刪除課程
+  @GetMapping(value = "/deleteclass/{cid}")
+  public void deleteClass(@PathVariable int cid) {
+    cbs.deleteById(cid);
+  }
+
 
 }
