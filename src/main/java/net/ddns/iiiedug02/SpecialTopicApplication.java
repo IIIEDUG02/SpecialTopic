36e0b3c508333ce6488864ff7e407cd1b7912028
1+ <nilm987521@gmail.com>
2022-03-15 09:05:01 +0800

    打包測試

--- src/main/java/net/ddns/iiiedug02/SpecialTopicApplication.java
+++ src/main/java/net/ddns/iiiedug02/SpecialTopicApplication.java
@@ -2,9 +2,16 @@ package net.ddns.iiiedug02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpecialTopicApplication {
+public class SpecialTopicApplication extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(SpringApplicationBuilder.class);
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(SpecialTopicApplication.class, args);
