736a4eb6d7bc1b5ae307ea3ed9388c40a8ce07f7
gt6678 <gt6678@gmail.com>
2022-03-15 23:03:59 +0800

    方法cid改為uid

--- src/main/java/net/ddns/iiiedug02/model/service/ClassBeanService.java
+++ src/main/java/net/ddns/iiiedug02/model/service/ClassBeanService.java
@@ -41,7 +41,7 @@ public class ClassBeanService {
 		return op1.get();
 	}
 	
-	public List<ClassBean> findAllByCid(Integer cid){
-		return cRepo.findAllByCid(cid);
+	public List<ClassBean> findAllByUid(Integer uid){
+		return cRepo.findAllByUid(uid);
 	}
 }
