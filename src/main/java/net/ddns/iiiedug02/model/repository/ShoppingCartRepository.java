9ebd7c9e49795d3d4ded0afa601901a31045c817
1+ <nilm987521@gmail.com>
2022-03-28 11:25:31 +0800

    新增購物車功能

---
+++ src/main/java/net/ddns/iiiedug02/model/repository/ShoppingCartRepository.java
@@ -0,0 +1,8 @@
+package net.ddns.iiiedug02.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import net.ddns.iiiedug02.model.bean.ShoppingCart;
+
+public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Integer> {
+
+}
