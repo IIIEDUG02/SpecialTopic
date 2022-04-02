421b8edb9c3039013642326157802885345599d8
1+ <nilm987521@gmail.com>
2022-04-02 20:51:48 +0800

    調整縮排

--- src/main/java/net/ddns/iiiedug02/controller/MPteacherController.java
+++ src/main/java/net/ddns/iiiedug02/controller/MPteacherController.java
@@ -12,7 +12,6 @@ import net.ddns.iiiedug02.model.bean.ClassBean;
 import net.ddns.iiiedug02.model.bean.MPteacher;
 import net.ddns.iiiedug02.model.bean.Member;
 import net.ddns.iiiedug02.model.bean.MemberInformation;
-import net.ddns.iiiedug02.model.service.CashService;
 import net.ddns.iiiedug02.model.service.ClassBeanService;
 import net.ddns.iiiedug02.model.service.ClassManagementService;
 import net.ddns.iiiedug02.model.service.MPteacherService;
@@ -22,64 +21,61 @@ import net.ddns.iiiedug02.model.service.MemberService;
 
 @Controller
 public class MPteacherController {
-  @Autowired
-  private MPteacherService mpteacherService;
+    @Autowired
+    private MPteacherService mpteacherService;
 
-  @Autowired
-  private CashService cashService;
+    @Autowired
+    private ClassBeanService classbeanService;
 
-  @Autowired
-  private ClassBeanService classbeanService;
+    @Autowired
+    private MemberService memberService;
 
-  @Autowired
-  private MemberService memberService;
-  
-  @Autowired
-  private ClassManagementService classMService;
+    @Autowired
+    private ClassManagementService classMService;
 
-  @GetMapping("/mpteacherquerybyid.controller")
-  public MPteacher processQueryByIdAction(Integer id) {
-    return mpteacherService.findById(id);
-  }
-
-  @GetMapping("/mpteachersavetop5")
-  public String processSaveTop5(Model m) {
-    List<Map<String, Integer>> cList = classMService.getMonthTop5Class(2022, 3);
-
-    List<MPteacher> mpteacherList = new ArrayList<MPteacher>();
-    for (Map<String, Integer> c : cList) {
-      ClassBean cbBean = classbeanService.findById(c.get("cid"));
-      Member mb = memberService.findByUid(cbBean.getUid());
-      MPteacher mpteacher = new MPteacher();
-      mpteacher.setMemberInformation(mb.getMemberInformation());
-      mpteacher.setMonth(3);
-      mpteacher.setMonthAmount(c.get("countcid"));
-      mpteacherService.insert(mpteacher);
-
-
-      mpteacherList.add(mpteacher);
+    @GetMapping("/mpteacherquerybyid.controller")
+    public MPteacher processQueryByIdAction(Integer id) {
+        return mpteacherService.findById(id);
     }
 
-    m.addAttribute("mpteacherList", mpteacherList);
-    return "success/Success2";
-  }
+    @GetMapping("/mpteachersavetop5")
+    public String processSaveTop5(Model m) {
+        List<Map<String, Integer>> cList = classMService.getMonthTop5Class(2022, 3);
 
-  @GetMapping("/mpteacherfindtop5")
-  @ResponseBody
-  public List<MemberInformation> processFindTop5(Model m) {
-    List<MemberInformation> memberinfoList = new ArrayList<MemberInformation>();
+        List<MPteacher> mpteacherList = new ArrayList<MPteacher>();
+        for (Map<String, Integer> c : cList) {
+            ClassBean cbBean = classbeanService.findById(c.get("cid"));
+            Member mb = memberService.findByUid(cbBean.getUid());
+            MPteacher mpteacher = new MPteacher();
+            mpteacher.setMemberInformation(mb.getMemberInformation());
+            mpteacher.setMonth(3);
+            mpteacher.setMonthAmount(c.get("countcid"));
+            mpteacherService.insert(mpteacher);
 
-    List<MPteacher> mpteacherList = mpteacherService.findAll();
-    for (MPteacher mp : mpteacherList) {
-      MemberInformation mbinfo = mp.getMemberInformation();
-      mbinfo.setMember(null);
-      memberinfoList.add(mbinfo);
+
+            mpteacherList.add(mpteacher);
+        }
+
+        m.addAttribute("mpteacherList", mpteacherList);
+        return "success/Success2";
     }
 
-    return memberinfoList;
+    @GetMapping("/mpteacherfindtop5")
+    @ResponseBody
+    public List<MemberInformation> processFindTop5(Model m) {
+        List<MemberInformation> memberinfoList = new ArrayList<MemberInformation>();
+
+        List<MPteacher> mpteacherList = mpteacherService.findAll();
+        for (MPteacher mp : mpteacherList) {
+            MemberInformation mbinfo = mp.getMemberInformation();
+            mbinfo.setMember(null);
+            memberinfoList.add(mbinfo);
+        }
+
+        return memberinfoList;
 
 
 
-  }
+    }
 
 }
--- src/main/java/net/ddns/iiiedug02/controller/StudentAnalController.java
+++ src/main/java/net/ddns/iiiedug02/controller/StudentAnalController.java
@@ -1,7 +1,5 @@
 package net.ddns.iiiedug02.controller;
 
-
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -11,7 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import net.ddns.iiiedug02.model.bean.ClassBean;
 import net.ddns.iiiedug02.model.bean.StudentAnalysis;
-import net.ddns.iiiedug02.model.service.CashService;
 import net.ddns.iiiedug02.model.service.ClassBeanService;
 import net.ddns.iiiedug02.model.service.ClassManagementService;
 import net.ddns.iiiedug02.model.service.StudentAnalService;
@@ -19,76 +16,73 @@ import net.ddns.iiiedug02.model.service.StudentAnalService;
 @Controller
 public class StudentAnalController {
 
-  @Autowired
-  private CashService cashService;
+    @Autowired
+    private ClassBeanService classBeanService;
 
-  @Autowired
-  private ClassBeanService classBeanService;
+    @Autowired
+    private StudentAnalService analyService;
 
-  @Autowired
-  private StudentAnalService analyService;
-  
-  @Autowired
-  private ClassManagementService classMService;
+    @Autowired
+    private ClassManagementService classMService;
 
-  @GetMapping("/calAverageAge")
-  public String processSaveTop5(Model m) {
-    List<Map<String, Integer>> averageAgeList = classMService.getAverageAge();
-    List<StudentAnalysis> studentAnalList = new ArrayList<StudentAnalysis>();
+    @GetMapping("/calAverageAge")
+    public String processSaveTop5(Model m) {
+        List<Map<String, Integer>> averageAgeList = classMService.getAverageAge();
+        List<StudentAnalysis> studentAnalList = new ArrayList<StudentAnalysis>();
 
-    for (Map<String, Integer> a : averageAgeList) {
-      StudentAnalysis stuanal = new StudentAnalysis();
+        for (Map<String, Integer> a : averageAgeList) {
+            StudentAnalysis stuanal = new StudentAnalysis();
 
-      ClassBean cb = classBeanService.findById(a.get("cid"));
-      stuanal.setAverageAge(a.get("avgAge"));
-      stuanal.setClasstitle(cb.getTitle());
-      stuanal.setJob(null);
-      
-      stuanal.setClassBean(cb);
-      analyService.insert(stuanal);
+            ClassBean cb = classBeanService.findById(a.get("cid"));
+            stuanal.setAverageAge(a.get("avgAge"));
+            stuanal.setClasstitle(cb.getTitle());
+            stuanal.setJob(null);
 
-      studentAnalList.add(stuanal);
+            stuanal.setClassBean(cb);
+            analyService.insert(stuanal);
 
+            studentAnalList.add(stuanal);
+
+        }
+
+
+        m.addAttribute("studentAnalList", studentAnalList);
+        return "success/Success5";
     }
 
+    // @GetMapping("/ypclassfindtop5")
+    // @ResponseBody
+    // public List<ClassBean> processFindTop5(Model m) {
+    // List<ClassBean> classBeanList = new ArrayList<ClassBean>();
+    //
+    // List<YPclass> YPclassList = ypclassService.findAll();
+    // for (YPclass c : YPclassList) {
+    // ClassBean classbean = classBeanService.findById(c.getClassID());
+    //// classbean.setClassDetailsBean(null);
+    // classbean.setCurriculumbean(null);
+    //
+    // classBeanList.add(classbean);
+    // }
+    //
+    // return classBeanList;
+    //
+    // }
 
-    m.addAttribute("studentAnalList", studentAnalList);
-    return "success/Success5";
-  }
-
-  // @GetMapping("/ypclassfindtop5")
-  // @ResponseBody
-  // public List<ClassBean> processFindTop5(Model m) {
-  // List<ClassBean> classBeanList = new ArrayList<ClassBean>();
-  //
-  // List<YPclass> YPclassList = ypclassService.findAll();
-  // for (YPclass c : YPclassList) {
-  // ClassBean classbean = classBeanService.findById(c.getClassID());
-  //// classbean.setClassDetailsBean(null);
-  // classbean.setCurriculumbean(null);
-  //
-  // classBeanList.add(classbean);
-  // }
-  //
-  // return classBeanList;
-  //
-  // }
-
-  // @GetMapping("/ypclassfindtop5")
-  // @ResponseBody
-  // public List<ClassBean> processFindTop5(Model m) {
-  // List<ClassBean> classBeanList = new ArrayList<ClassBean>();
-  //
-  // List<YPclass> YPclassList = ypclassService.findAll();
-  // for (YPclass c : YPclassList) {
-  // ClassBean classBean = c.getClassBean();
-  // classBean.setCurriculumbean(null);
-  //
-  // classBeanList.add(classBean);
-  // }
-  //
-  // return classBeanList;
-  //
-  // }
+    // @GetMapping("/ypclassfindtop5")
+    // @ResponseBody
+    // public List<ClassBean> processFindTop5(Model m) {
+    // List<ClassBean> classBeanList = new ArrayList<ClassBean>();
+    //
+    // List<YPclass> YPclassList = ypclassService.findAll();
+    // for (YPclass c : YPclassList) {
+    // ClassBean classBean = c.getClassBean();
+    // classBean.setCurriculumbean(null);
+    //
+    // classBeanList.add(classBean);
+    // }
+    //
+    // return classBeanList;
+    //
+    // }
 
 }
