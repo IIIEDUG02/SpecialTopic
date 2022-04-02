8f5eceb1b8089f3ae81f662eaa8cadc97087288b
1+ <nilm987521@gmail.com>
2022-04-02 23:05:04 +0800

    移動說明位置

--- src/main/java/net/ddns/iiiedug02/exception/NotLoginException.java
+++ src/main/java/net/ddns/iiiedug02/exception/NotLoginException.java
@@ -3,11 +3,12 @@ package net.ddns.iiiedug02.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * 使用者未登入的Exception
+ */
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class NotLoginException extends RuntimeException {
-    /**
-     * 使用者未登入的Exception
-     */
+
     private static final long serialVersionUID = 1L;
 
     public NotLoginException() {
