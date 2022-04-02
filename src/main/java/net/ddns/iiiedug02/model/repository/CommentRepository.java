bae3a978c7d37fcf86c204d655751e36d6b1b6f9
pandachan333 <r545945w2@gmail.com>
2022-04-02 22:23:37 +0800

    更新前端刪除文章時會跳視窗提醒是否確定要刪除文章

--- src/main/java/net/ddns/iiiedug02/model/repository/CommentRepository.java
+++ src/main/java/net/ddns/iiiedug02/model/repository/CommentRepository.java
@@ -9,7 +9,7 @@ import net.ddns.iiiedug02.model.bean.Comment;
 
 // 能夠分頁及排序的 Repository
 public interface CommentRepository extends PagingAndSortingRepository<Comment, Long> {
-  @Query("FROM Comment WHERE ClassBean.cid = :cid AND type = :type")
+  @Query("FROM Comment WHERE classBean.cid = :cid AND type = :type")
   List<Comment> getCommentsByClassCidAndCommentType(int cid, String type);
   List<Comment> findByUuid(String uuid);
   long deleteByUuid(String uuid);
