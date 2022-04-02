becf2e042e4d3feab5711171368a385808a04a77
1+ <nilm987521@gmail.com>
2022-04-02 20:49:47 +0800

    拿掉 getLoginStatus

--- src/main/resources/static/js/loginjs.js
+++ src/main/resources/static/js/loginjs.js
@@ -1,28 +1,3 @@
-var xmlhttp = new XMLHttpRequest();
-var url = "/SpecialTopic/getLoginStatus";
-
-xmlhttp.onreadystatechange = function() {
-	if (this.readyState == 4 && this.status == 200) {
-		var data = JSON.parse(this.responseText);
-		indexRender(data);
-	}
-};
-xmlhttp.open("GET", url, false);
-xmlhttp.send();
-
-
-function indexRender(data) {
-	if (data == null) {
-		$("li#dp").remove();
-	} else if (data.username != "null") {
-		$("a#login").remove();
-		$("a#register").remove();		
-	} else {
-		$("li#dp").remove();
-	}
-
-}
-
 function showLoginForm() {
 	$('div#loginform').modal('toggle');
 }
