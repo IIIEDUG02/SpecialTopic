3ffb091a2b883304c122effd87744a05c58b44aa
1+ <nilm987521@gmail.com>
2022-04-02 23:04:41 +0800

    新增購物車的exception

--- src/main/java/net/ddns/iiiedug02/exception/SystemExceptionHandler.java
+++ src/main/java/net/ddns/iiiedug02/exception/SystemExceptionHandler.java
@@ -1,22 +1,28 @@
 package net.ddns.iiiedug02.exception;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
 /*
- * 須從controller拋出Exception，才會觸發必須使用 ModelAndView ，用Model無法傳送訊息
+ * 須從controller拋出Exception，才會觸發，必須使用ModelAndView，用Model無法傳送訊息
  */
 @ControllerAdvice()
 public class SystemExceptionHandler {
 
     @ExceptionHandler(NotLoginException.class)
-    public ModelAndView noLoginException(NotLoginException e, Model m) {
+    public ModelAndView noLoginException(NotLoginException e) {
         ModelAndView mav = new ModelAndView();
         mav.addObject("errMsg", e.getMessage());
         mav.setViewName("index");
         return mav;
     }
 
+    @ExceptionHandler(ItemInCartException.class)
+    public ModelAndView itemInCartException(ItemInCartException e) {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("msg", e.getMessage());
+        mav.setViewName("tradeRecord/shopping_cart_info");
+        return mav;
+    }
 }
