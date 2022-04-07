33d358d6b2817e83cd90feee6ce20b366cff3115
1+ <nilm987521@gmail.com>
2022-04-08 05:42:01 +0800

    更新gitbook

--- src/main/java/net/ddns/iiiedug02/controller/ClassController.java
+++ src/main/java/net/ddns/iiiedug02/controller/ClassController.java
@@ -7,11 +7,8 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -39,21 +36,21 @@ import net.ddns.iiiedug02.util.UniversalTool;
 @Controller
 @SessionAttributes({"classbean"})
 public class ClassController {
-	@Autowired
-	private CurriculumService cus;
+    @Autowired
+    private CurriculumService cus;
 
-	@Autowired
-	private ClassBeanService cbs;
+    @Autowired
+    private ClassBeanService cbs;
 
-	@Autowired
-	private MemberService ms;
+    @Autowired
+    private MemberService ms;
 
-	@Autowired
-	private ClassOnlineService cos;
+    @Autowired
+    private ClassOnlineService cos;
 
-	@Autowired
-	private ClassManagementService cms;
-	
+    @Autowired
+    private ClassManagementService cms;
+
 
     @Autowired
     private UniversalTool utool;
@@ -111,40 +108,38 @@ public class ClassController {
     public String uploadPhoto(@RequestParam("myPhoto") MultipartFile mf, HttpServletRequest request,
             Model m) throws IllegalStateException, IOException {
 
-	// 課程圖片上傳
-	@PostMapping(path = "/uploadphoto")
-	@ResponseBody
-	public String uploadPhoto(@RequestParam("myPhoto") MultipartFile mf, HttpServletRequest request, Model m)
-			throws IllegalStateException, IOException {
+        String pattern = "yyyy-MM-dd-HH-mm-ss";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
 
-		String pattern = "yyyy-MM-dd-HH-mm-ss";
-		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        Random random = new Random();
+        int rNumber = 10000 + random.nextInt(90000);
+        // 取副檔名方法一
+        // String oName = mf.getOriginalFilename();
+        // int oNameLenghth = oName.length();
+        // String fileName =
+        // simpleDateFormat.format(new Date()) + "-" + rNumber +
+        // oName.substring(oNameLenghth-4,
+        // oNameLenghth);
+        String type = FilenameUtils.getExtension(mf.getOriginalFilename());
+        if (type.isEmpty()) {
+            return "no image";
+        }
+        String fileName = simpleDateFormat.format(new Date()) + "-" + rNumber + "." + type;
 
-		Random random = new Random();
-		int rNumber = 10000 + random.nextInt(90000);
-		// 取副檔名方法一
-		// String oName = mf.getOriginalFilename();
-		// int oNameLenghth = oName.length();
-		// String fileName =
-		// simpleDateFormat.format(new Date()) + "-" + rNumber +
-		// oName.substring(oNameLenghth-4,
-		// oNameLenghth);
-		String type = FilenameUtils.getExtension(mf.getOriginalFilename());
-		if (type.isEmpty()) {
-			return "no image";
-		}
-		String fileName = simpleDateFormat.format(new Date()) + "-" + rNumber + "." + type;
+        String tempDir = request.getSession().getServletContext().getRealPath("/")
+                + "../resources/static/classphoto//";
+        File tempDirFile = new File(tempDir);
+        tempDirFile.mkdirs();
 
-		String tempDir = request.getSession().getServletContext().getRealPath("/") + "../resources/static/classphoto//";
-		File tempDirFile = new File(tempDir);
-		tempDirFile.mkdirs();
+        String saveFilePath = tempDir + fileName;
+        File saveFile = new File(saveFilePath);
 
-		String saveFilePath = tempDir + fileName;
-		File saveFile = new File(saveFilePath);
+        mf.transferTo(saveFile);
 
-		mf.transferTo(saveFile);
+        m.addAttribute("photopath", "/SpecialTopic/classphoto/" + fileName);
 
-		m.addAttribute("photopath", "/SpecialTopic/classphoto/" + fileName);
+        return "";
+    }
 
     // 尋找全部上線課程,api
     @GetMapping(path = "class/allonline")
@@ -201,12 +196,5 @@ public class ClassController {
         }
 
     }
-
-	@GetMapping("getCurListJson/api/{cid}")
-	@ResponseBody
-	public List<CurriculumBean> getCurListJson(Model m, @PathVariable("cid") int cid) {
-		ClassBean cb = cbs.findById(cid);
-		return cus.findAllByClassbean(cb);
-	}
 
 }
