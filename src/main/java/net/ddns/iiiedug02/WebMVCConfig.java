1fcdbf5162abd5d7fef83ef8271b7aaab17b4cb8
1+ <nilm987521@gmail.com>
2022-04-09 15:00:42 +0800

    帳號密碼錯誤提示

--- src/main/java/net/ddns/iiiedug02/WebMVCConfig.java
+++ src/main/java/net/ddns/iiiedug02/WebMVCConfig.java
@@ -1,8 +1,15 @@
 package net.ddns.iiiedug02;
 
+
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
 public class WebMVCConfig implements WebMvcConfigurer {
@@ -25,4 +32,17 @@ public class WebMVCConfig implements WebMvcConfigurer {
         registry.addViewController("/contact").setViewName("contact");
     }
 
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        registry.addInterceptor(localeChangeInterceptor);
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
+        return cookieLocaleResolver;
+    }
 }
