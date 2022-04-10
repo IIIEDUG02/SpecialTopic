3bd66acd60caf6186182d73ec9f65e99c190fa39
gt6678 <gt6678@gmail.com>
2022-04-10 17:50:43 +0800

    無章節時無畫面問題解決

--- src/main/java/net/ddns/iiiedug02/controller/ClassController.java
+++ src/main/java/net/ddns/iiiedug02/controller/ClassController.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -39,287 +40,281 @@ import net.ddns.iiiedug02.model.service.MemberService;
 import net.ddns.iiiedug02.util.UniversalTool;
 
 @Controller
-@SessionAttributes({"classbean"})
+@SessionAttributes({ "classbean" })
 public class ClassController {
-    @Autowired
-    private CurriculumService cus;
+	@Autowired
+	private CurriculumService cus;
 
-    @Autowired
-    private ClassBeanService cbs;
+	@Autowired
+	private ClassBeanService cbs;
 
-    @Autowired
-    private MemberService ms;
+	@Autowired
+	private MemberService ms;
 
-    @Autowired
-    private ClassOnlineService cos;
+	@Autowired
+	private ClassOnlineService cos;
+	
+	@Autowired
+	private ResourceLoader resourceLoader;
 
-    @Autowired
-    private ClassManagementService cms;
+	@Autowired
+	private ClassManagementService cms;
 
+	@Autowired
+	private UniversalTool utool;
 
-    @Autowired
-    private UniversalTool utool;
+	/*
+	 * 進入後臺管理課程，區分admin跟normal身份
+	 */
+	@GetMapping("class/list")
+	public String dispatch(HttpServletRequest request, Principal principal, Model model) {
+		Member loginBean = ms.findByUsername(principal.getName());
+		if (utool.hasRole(principal, "admin")) {
+			List<ClassBean> cbList = cbs.findAll();
+			model.addAttribute("allCbList", cbList);
+			return "class/classList";
+		} else if (utool.hasRole(principal, "normal")) {
+			List<ClassManagementBean> cmbList = cms.findByUid(loginBean.getUid());
+			List<ClassBean> completeList = new ArrayList<ClassBean>();
+			List<ClassBean> uncompleteList = new ArrayList<ClassBean>();
 
-    /*
-     * 進入後臺管理課程，區分admin跟normal身份
-     */
-    @GetMapping("class/list")
-    public String dispatch(HttpServletRequest request, Principal principal, Model model) {
-        Member loginBean = ms.findByUsername(principal.getName());
-        if (utool.hasRole(principal, "admin")) {
-            List<ClassBean> cbList = cbs.findAll();
-            model.addAttribute("allCbList", cbList);
-            return "class/classList";
-        } else if (utool.hasRole(principal, "normal")) {
-            List<ClassManagementBean> cmbList = cms.findByUid(loginBean.getUid());
-            List<ClassBean> completeList = new ArrayList<ClassBean>();
-            List<ClassBean> uncompleteList = new ArrayList<ClassBean>();
+			if (!cmbList.isEmpty()) {
+				for (ClassManagementBean cmb : cmbList) {
+					ClassBean cb = cbs.findById(cmb.getCid());
+					if (cmb.getStatus() == 1) {
+						completeList.add(cb);
+					} else {
+						uncompleteList.add(cb);
+					}
+				}
+				model.addAttribute("uncompleteList", uncompleteList);
+				model.addAttribute("completeList", completeList);
+			}
+			return "class/classList";
+		}
+		throw new RoleNotFoundException();
+	}
 
-            if (!cmbList.isEmpty()) {
-                for (ClassManagementBean cmb : cmbList) {
-                    ClassBean cb = cbs.findById(cmb.getCid());
-                    if (cmb.getStatus() == 1) {
-                        completeList.add(cb);
-                    } else {
-                        uncompleteList.add(cb);
-                    }
-                }
-                model.addAttribute("uncompleteList", uncompleteList);
-                model.addAttribute("completeList", completeList);
-            }
-            return "class/classList";
-        }
-        throw new RoleNotFoundException();
-    }
+	// 進入編輯課程頁面
+	@GetMapping(value = "class/update/{cid}")
+	public String updatePage(@PathVariable int cid, Model model) {
+		ClassBean cb = cbs.findById(cid);
+		model.addAttribute("classBean", cb);
+		return "backstage/classEdit";
+	}
 
-    // 進入編輯課程頁面
-    @GetMapping(value = "class/update/{cid}")
-    public String updatePage(@PathVariable int cid, Model model) {
-        ClassBean cb = cbs.findById(cid);
-        model.addAttribute("classBean", cb);
-        return "backstage/classEdit";
-    }
+	// 創建class
+	@PostMapping(value = "/createclass", produces = "text/html;charset=utf-8")
+	public String createProcessClass(@RequestParam Map<String, String> formData, Model model) {
+		ClassBean cb = new ClassBean();
+		cb.setClassType(formData.get("classtype"));
+		cb.setTitle(formData.get("classtitle"));
+		cb.setPrice(Integer.parseInt(formData.get("classprice")));
+		cb.setUid(Integer.parseInt(formData.get("teacherid")));
+		model.addAttribute("classbean", cb);
 
-    // 創建class
-    @PostMapping(value = "/createclass", produces = "text/html;charset=utf-8")
-    public String createProcessClass(@RequestParam Map<String, String> formData, Model model) {
-        ClassBean cb = new ClassBean();
-        cb.setClassType(formData.get("classtype"));
-        cb.setTitle(formData.get("classtitle"));
-        cb.setPrice(Integer.parseInt(formData.get("classprice")));
-        cb.setUid(Integer.parseInt(formData.get("teacherid")));
-        model.addAttribute("classbean", cb);
+		return "class/createclassdetails";
+	}
 
-        return "class/createclassdetails";
-    }
+	// 創建classdetails
+	@PostMapping(value = "/createclassdetails")
+	public String createProcessClassDetails(@RequestParam Map<String, String> formData, HttpSession session) {
+		ClassDetailsBean cdb = new ClassDetailsBean();
+		cdb.setDescript(formData.get("descript"));
+		cdb.setGoal(formData.get("goal"));
+		cdb.setNeed_tool(formData.get("needed_tool"));
+		cdb.setStu_required(formData.get("stu_required"));
+		cdb.setVideo(formData.get("video"));
+		cdb.setLength_min(Integer.parseInt(formData.get("length_min")));
+		ClassBean cb = (ClassBean) session.getAttribute("classbean");
+		cb.setClassDetailsBean(cdb);
+		cdb.setClassbean(cb);
+		cbs.insert(cb);
+		return "class/createcurriculum";
+	}
 
-    // 創建classdetails
-    @PostMapping(value = "/createclassdetails")
-    public String createProcessClassDetails(@RequestParam Map<String, String> formData,
-            HttpSession session) {
-        ClassDetailsBean cdb = new ClassDetailsBean();
-        cdb.setDescript(formData.get("descript"));
-        cdb.setGoal(formData.get("goal"));
-        cdb.setNeed_tool(formData.get("needed_tool"));
-        cdb.setStu_required(formData.get("stu_required"));
-        cdb.setVideo(formData.get("video"));
-        cdb.setLength_min(Integer.parseInt(formData.get("length_min")));
-        ClassBean cb = (ClassBean) session.getAttribute("classbean");
-        cb.setClassDetailsBean(cdb);
-        cdb.setClassbean(cb);
-        cbs.insert(cb);
-        return "class/createcurriculum";
-    }
+	// 創建curriculum
+	@PostMapping(path = "/createcurriculum")
+	public String createCurriculum(@RequestParam("myVideo") MultipartFile mf, HttpServletRequest request, Model m,
+			HttpSession session, @SessionAttribute ClassBean classbean, Principal p)
+			throws IllegalStateException, IOException {
 
-    // 創建curriculum
-    @PostMapping(path = "/createcurriculum")
-    public String createCurriculum(@RequestParam("myVideo") MultipartFile mf, HttpServletRequest request,
-            Model m, HttpSession session, @SessionAttribute ClassBean classbean,Principal p)
-            throws IllegalStateException, IOException {
+		String pattern = "yyyy-MM-dd-HH-mm-ss";
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
 
-        String pattern = "yyyy-MM-dd-HH-mm-ss";
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+		Random random = new Random();
+		int rNumber = 10000 + random.nextInt(90000);
 
-        Random random = new Random();
-        int rNumber = 10000 + random.nextInt(90000);
+		String type = FilenameUtils.getExtension(mf.getOriginalFilename());
+		if (type.isEmpty()) {
+			return "no video";
+		}
+		String fileName = simpleDateFormat.format(new Date()) + "-" + rNumber + "." + type;
+		String tempDir = resourceLoader.getResource("classpath:static/").getFile().toString()+"/classvideo/";
+		//String tempDir = request.getSession().getServletContext().getRealPath("/") + "/classvideo//";
+		File tempDirFile = new File(tempDir);
+		tempDirFile.mkdirs();
 
-        String type = FilenameUtils.getExtension(mf.getOriginalFilename());
-        if (type.isEmpty()) {
-            return "no video";
-        }
-        String fileName = simpleDateFormat.format(new Date()) + "-" + rNumber + "." + type;
+		String saveFilePath = tempDir + fileName;
+		File saveFile = new File(saveFilePath);
 
-        String tempDir = request.getSession().getServletContext().getRealPath("/")
-                + "../resources/static/classvideo//";
-        File tempDirFile = new File(tempDir);
-        tempDirFile.mkdirs();
-
-        String saveFilePath = tempDir + fileName;
-        File saveFile = new File(saveFilePath);
-
-        mf.transferTo(saveFile);
-        CurriculumBean cub = new CurriculumBean();
+		mf.transferTo(saveFile);
+		CurriculumBean cub = new CurriculumBean();
 
 		ClassBean cb = (ClassBean) m.getAttribute("classbean");
 		cub.setClassbean(cb);
 		cub.setChapter(request.getParameter("chapter"));
 		cub.setVideo_path("/SpecialTopic/classvideo/" + fileName);
 		cus.insert(cub);
-	
+
 		return this.editCurriculum(request, p, cb.getCid(), m);
 	}
-    
-    // 創建curriculum
-    @PostMapping(path = "/updatecurriculum")
-    public String updateCurriculum(@RequestParam("myVideo") MultipartFile mf, HttpServletRequest request,
-            Model m, HttpSession session, @SessionAttribute ClassBean classbean,Principal p)
-            throws IllegalStateException, IOException {
 
-        String pattern = "yyyy-MM-dd-HH-mm-ss";
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+	// 創建curriculum
+	@PostMapping(path = "/updatecurriculum")
+	public String updateCurriculum(@RequestParam("myVideo") MultipartFile mf, HttpServletRequest request, Model m,
+			HttpSession session, @SessionAttribute ClassBean classbean, Principal p)
+			throws IllegalStateException, IOException {
 
-        Random random = new Random();
-        int rNumber = 10000 + random.nextInt(90000);
+		String pattern = "yyyy-MM-dd-HH-mm-ss";
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
 
-        String type = FilenameUtils.getExtension(mf.getOriginalFilename());
-        if (type.isEmpty()) {
-            return "no video";
-        }
-        String fileName = simpleDateFormat.format(new Date()) + "-" + rNumber + "." + type;
+		Random random = new Random();
+		int rNumber = 10000 + random.nextInt(90000);
 
-        String tempDir = request.getSession().getServletContext().getRealPath("/")
-                + "../resources/static/classvideo//";
-        File tempDirFile = new File(tempDir);
-        tempDirFile.mkdirs();
+		String type = FilenameUtils.getExtension(mf.getOriginalFilename());
+		if (type.isEmpty()) {
+			return "no video";
+		}
+		String fileName = simpleDateFormat.format(new Date()) + "-" + rNumber + "." + type;
 
-        String saveFilePath = tempDir + fileName;
-        File saveFile = new File(saveFilePath);
+		String tempDir = request.getSession().getServletContext().getRealPath("/") + "../resources/static/classvideo//";
+		File tempDirFile = new File(tempDir);
+		tempDirFile.mkdirs();
 
-        mf.transferTo(saveFile);
-        CurriculumBean cub = cus.findById(Integer.parseInt(request.getParameter("cuid")));
+		String saveFilePath = tempDir + fileName;
+		File saveFile = new File(saveFilePath);
+
+		mf.transferTo(saveFile);
+		CurriculumBean cub = cus.findById(Integer.parseInt(request.getParameter("cuid")));
 
 		ClassBean cb = (ClassBean) m.getAttribute("classbean");
 		cub.setClassbean(cb);
 		cub.setChapter(request.getParameter("chapter"));
 		cub.setVideo_path("/SpecialTopic/classvideo/" + fileName);
 		cus.insert(cub);
-	
+
 		return this.editCurriculum(request, p, cb.getCid(), m);
 	}
-	
-    
 
-    // 計算全部上線課程總數
-    @ResponseBody
-    @GetMapping(value = "/countclass")
-    public int processCountClass() {
-        return cbs.countClass();
-    }
+	// 計算全部上線課程總數
+	@ResponseBody
+	@GetMapping(value = "/countclass")
+	public int processCountClass() {
+		return cbs.countClass();
+	}
 
-    // 課程圖片上傳 api
-    @PostMapping(path = "class/api/uploadphoto")
-    @ResponseBody
-    public String uploadPhoto(@RequestParam("myPhoto") MultipartFile mf, HttpServletRequest request,
-            Model m) throws IllegalStateException, IOException {
+	// 課程圖片上傳 api
+	@PostMapping(path = "class/api/uploadphoto")
+	@ResponseBody
+	public String uploadPhoto(@RequestParam("myPhoto") MultipartFile mf, HttpServletRequest request, Model m)
+			throws IllegalStateException, IOException {
 
-        String pattern = "yyyy-MM-dd-HH-mm-ss";
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+		String pattern = "yyyy-MM-dd-HH-mm-ss";
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
 
-        Random random = new Random();
-        int rNumber = 10000 + random.nextInt(90000);
+		Random random = new Random();
+		int rNumber = 10000 + random.nextInt(90000);
 
-        String type = FilenameUtils.getExtension(mf.getOriginalFilename());
-        if (type.isEmpty()) {
-            return "no image";
-        }
-        String fileName = simpleDateFormat.format(new Date()) + "-" + rNumber + "." + type;
+		String type = FilenameUtils.getExtension(mf.getOriginalFilename());
+		if (type.isEmpty()) {
+			return "no image";
+		}
+		String fileName = simpleDateFormat.format(new Date()) + "-" + rNumber + "." + type;
 
-        String tempDir = request.getSession().getServletContext().getRealPath("/")
-                + "../resources/static/classphoto//";
-        File tempDirFile = new File(tempDir);
-        tempDirFile.mkdirs();
+		String tempDir = request.getSession().getServletContext().getRealPath("/") + "../resources/static/classphoto//";
+		File tempDirFile = new File(tempDir);
+		tempDirFile.mkdirs();
 
-        String saveFilePath = tempDir + fileName;
-        File saveFile = new File(saveFilePath);
+		String saveFilePath = tempDir + fileName;
+		File saveFile = new File(saveFilePath);
 
-        mf.transferTo(saveFile);
+		mf.transferTo(saveFile);
 
-        m.addAttribute("photopath", "/SpecialTopic/classphoto/" + fileName);
-        return "...";
-    }
+		m.addAttribute("photopath", "/SpecialTopic/classphoto/" + fileName);
+		return "...";
+	}
 
-    // 尋找全部上線課程,api
-    @GetMapping(path = "class/allonline")
-    @ResponseBody
-    public List<ClassBean> findAllOnlineClass() {
-        List<ClassOnlineBean> coList = cos.findAll();
-        List<ClassBean> cbList = new ArrayList<ClassBean>();
-        for (ClassOnlineBean co : coList) {
-            ClassBean cb = cbs.findById(co.getCid());
-            cb.setClassDetailsBean(null);
-            cb.setCurriculumbean(null);
-            cbList.add(cb);
-        }
-        return cbList;
-    }
+	// 尋找全部上線課程,api
+	@GetMapping(path = "class/allonline")
+	@ResponseBody
+	public List<ClassBean> findAllOnlineClass() {
+		List<ClassOnlineBean> coList = cos.findAll();
+		List<ClassBean> cbList = new ArrayList<ClassBean>();
+		for (ClassOnlineBean co : coList) {
+			ClassBean cb = cbs.findById(co.getCid());
+			cb.setClassDetailsBean(null);
+			cb.setCurriculumbean(null);
+			cbList.add(cb);
+		}
+		return cbList;
+	}
 
-    @GetMapping("class/curriculums/{cid}")
-    public String classtest(Model m, @PathVariable("cid") int cid) {
-        ClassBean cb = cbs.findById(cid);
-        List<CurriculumBean> cusList = cus.findAllByClassbean(cb);
-        m.addAttribute("CurriculumList", cusList);
-        m.addAttribute("cid", cid);
-        return "class/curriculum-nilm";
-    }
+	@GetMapping("class/curriculums/{cid}")
+	public String classtest(Model m, @PathVariable("cid") int cid) {
+		ClassBean cb = cbs.findById(cid);
+		List<CurriculumBean> cusList = cus.findAllByClassbean(cb);
+		m.addAttribute("CurriculumList", cusList);
+		m.addAttribute("cid", cid);
+		return "class/curriculum-nilm";
+	}
 
-    @GetMapping("class/curriculums/api/{cid}")
-    @ResponseBody
-    public List<CurriculumBean> getCurListJson(Model m, @PathVariable("cid") int cid) {
-        ClassBean cb = cbs.findById(cid);
-        return cus.findAllByClassbean(cb);
-    }
+	@GetMapping("class/curriculums/api/{cid}")
+	@ResponseBody
+	public List<CurriculumBean> getCurListJson(Model m, @PathVariable("cid") int cid) {
+		ClassBean cb = cbs.findById(cid);
+		return cus.findAllByClassbean(cb);
+	}
 
-    @DeleteMapping("class/delete/{cid}")
-    @ResponseBody
-    public boolean deleteClass(HttpServletRequest request, Principal principal,
-            @PathVariable("cid") int cid) {
-        if (utool.hasRole(principal, "admin")) {
-            return cbs.deleteById(cid);
-        }
-        return false;
-    }
+	@DeleteMapping("class/delete/{cid}")
+	@ResponseBody
+	public boolean deleteClass(HttpServletRequest request, Principal principal, @PathVariable("cid") int cid) {
+		if (utool.hasRole(principal, "admin")) {
+			return cbs.deleteById(cid);
+		}
+		return false;
+	}
 
-    @DeleteMapping("class/delete/curriculum/{cuid}")
-    @ResponseBody
-    public boolean deleteCurriculum(HttpServletRequest request, Principal principal,
-            @PathVariable("cuid") int cuid) {
-        if (utool.hasRole(principal, "admin")) {
-            return cus.deleteById(cuid);
-        }
-        return false;
-    }
+	@DeleteMapping("class/delete/curriculum/{cuid}")
+	@ResponseBody
+	public boolean deleteCurriculum(HttpServletRequest request, Principal principal, @PathVariable("cuid") int cuid) {
+		if (utool.hasRole(principal, "admin")) {
+			return cus.deleteById(cuid);
+		}
+		return false;
+	}
 
-    @GetMapping("class/editCurriculum/{cid}")
-    public String editCurriculum(HttpServletRequest request, Principal principal,
-            @PathVariable("cid") int cid, Model m) {
-        if (utool.hasRole(principal, "admin")) {
-            ClassBean cb = cbs.findById(cid);
-            List<CurriculumBean> cubList = cus.findAllByClassbean(cb);
-            m.addAttribute("cubList", cubList);
-            m.addAttribute("classbean", cb);
-            return "class/editCurriculum";
-        } else {
-            m.addAttribute("errMsg", "您無權限執行此操作");
-            return "index";
-        }
+	@GetMapping("class/editCurriculum/{cid}")
+	public String editCurriculum(HttpServletRequest request, Principal principal, @PathVariable("cid") int cid,
+			Model m) {
+		if (utool.hasRole(principal, "admin")) {
+			ClassBean cb = cbs.findById(cid);
+			List<CurriculumBean> cubList = cus.findAllByClassbean(cb);
+			m.addAttribute("cubList", cubList);
+			m.addAttribute("classbean", cb);
+			return "class/editCurriculum";
+		} else {
+			m.addAttribute("errMsg", "您無權限執行此操作");
+			return "index";
+		}
 
-    }
+	}
 
-    @GetMapping("class/showClassType")
-    public List<String> showClassType(HttpServletRequest request, Model m) {
-        List<String> classTypeList = cbs.findAllClassType();
-        m.addAttribute("classTypeList", classTypeList);
-        return classTypeList;
-    }
+	@GetMapping("class/showClassType")
+	public List<String> showClassType(HttpServletRequest request, Model m) {
+		List<String> classTypeList = cbs.findAllClassType();
+		m.addAttribute("classTypeList", classTypeList);
+		return classTypeList;
+	}
 
 }
--- src/main/webapp/WEB-INF/views/class/createclass.jsp
+++ src/main/webapp/WEB-INF/views/class/createclass.jsp
@@ -29,10 +29,10 @@
 				<td><label>老師ID:</label><input type="text" id="teacherid"
 					name="teacherid" /></td>
 			</tr>
-<!-- 			<tr> -->
-<!-- 				<td><label>課程圖片:</label><input type="file" id="photopath" -->
-<!-- 					name="photopath" /></td> -->
-<!-- 			</tr> -->
+			<tr>
+				<td><label>課程圖片:</label><input type="file" id="photopath"
+					name="photopath" /></td>
+			</tr>
 			<tr>
 				<td><input type="submit" value="下一步" /> <input type="button"
 					onclick="javascript:window.location = '/'" value="返回首頁" /></td>
@@ -44,27 +44,27 @@
 			<div class="td"></div>
 	</div>
 	</div>
-<!-- 	<script> 
-// 		//預覽上傳照片(時機：選檔變更事件)
-// 		$("#phottpath").change(function() {
-// 			$("#preview .table .td").html("");//先清除前次預覽的<img>
-// 			previewImg(this.files);
-// 		});
-// 		function previewImg(files) {
-// 			if (files.length == 0)
-// 				return;
-// 			var file = files[0];
-// 			var fr = new FileReader();
-// 			//註冊：選檔被讀取完成後之事件處理器
-// 			fr.onload = function() {
-// 				var img = $("<img>").attr({
-// 					src : fr.result
-// 				});
+	<script> 
+ 		//預覽上傳照片(時機：選檔變更事件)
+ 		$("#phottpath").change(function() {
+ 			$("#preview .table .td").html("");//先清除前次預覽的<img>
+ 			previewImg(this.files);
+ 		});
+ 		function previewImg(files) {
+			if (files.length == 0)
+				return;
+			var file = files[0];
+ 			var fr = new FileReader();
+			//註冊：選檔被讀取完成後之事件處理器
+			fr.onload = function() {
+				var img = $("<img>").attr({
+ 					src : fr.result
+ 				});
 
-// 				$("#preview .table .td").html(img);
-// 			};
-// 			fr.readAsDataURL(file);
-// 		}
- 	</script> -->
+ 				$("#preview .table .td").html(img);
+ 			};
+ 			fr.readAsDataURL(file);
+ 		}
+ 	</script> 
 </body>
 </html>
