e087eda3683e5c4da6f03d9323f3208094b091b1
VICKY-W\hp <nilm987521@gmail.com>
2022-04-11 12:38:24 +0800

    修正無法新增問題

--- src/main/java/net/ddns/iiiedug02/model/bean/ProgressRecord.java
+++ src/main/java/net/ddns/iiiedug02/model/bean/ProgressRecord.java
@@ -1,6 +1,8 @@
 package net.ddns.iiiedug02.model.bean;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import org.springframework.stereotype.Component;
@@ -15,7 +17,8 @@ import lombok.Setter;
 public class ProgressRecord {
 
     @Id
-    private int id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id = 0;
 
     private int uid;
 
