1e0967daee8857ad59607434bed842cd69553ac0
1+ <nilm987521@gmail.com>
2022-04-15 12:21:41 +0800

    性別數字改文字

--- src/main/java/net/ddns/iiiedug02/model/bean/MemberInformation.java
+++ src/main/java/net/ddns/iiiedug02/model/bean/MemberInformation.java
@@ -1,7 +1,6 @@
 package net.ddns.iiiedug02.model.bean;
 
 import java.io.Serializable;
-import java.sql.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -24,34 +23,32 @@ import lombok.Setter;
 @Getter
 public class MemberInformation implements Serializable {
 
-  private static final long serialVersionUID = 2L;
+    private static final long serialVersionUID = 2L;
 
-  @Id
-  @OneToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "uid", referencedColumnName = "uid")
-  @JsonIgnore
-  private Member member;
+    @Id
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "uid", referencedColumnName = "uid")
+    @JsonIgnore
+    private Member member;
 
-  @Column(name = "address")
-  private String address = "";
-  @Column(name = "phone")
-  private String phone = "";
-  @Column(name = "fullname")
-  private String fullname = "";
-  @Column(name = "email")
-  private String email = "";
-  @Column(name = "birthday")
-  private String birthday;
-  @Column(name = "job")
-  private String job = "";
-  @Column(name = "photo")
-  private String photo;
-  @Column(name = "passportname")
-  private String passportname;
-  @Column(name = "identitycard")
-  private String identitycard;
-  @Column(name = "gender")
-  private Integer gender;
-
-
+    @Column(name = "address")
+    private String address = "";
+    @Column(name = "phone")
+    private String phone = "";
+    @Column(name = "fullname")
+    private String fullname = "";
+    @Column(name = "email")
+    private String email = "";
+    @Column(name = "birthday")
+    private String birthday;
+    @Column(name = "job")
+    private String job = "";
+    @Column(name = "photo")
+    private String photo;
+    @Column(name = "passportname")
+    private String passportname;
+    @Column(name = "identitycard")
+    private String identitycard;
+    @Column(name = "gender")
+    private String gender;
 }
